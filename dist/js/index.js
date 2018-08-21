fb571d1f20f2a0711afb32a9cd9592827fa9f93a
Zachary <zak.d.grant@gmail.com>
2018-08-21 19:33:17 -0400

    Finished Phase 1

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,3 +3,8 @@
 $(document).ready(function() {
   $(".sidenav").sidenav();
 });
+
+// $(document).addEventListener("DOMContentLoaded", function() {
+//   var elems = document.querySelectorAll(".sidenav");
+//   var instances = M.Sidenav.init(elems, options);
+// });
